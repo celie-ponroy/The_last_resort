38a7ab6f974083a01d5b75c979fcad0c2aab00c4
Nino <nino.arcelin@gmail.com>
2024-10-19 13:43:57 +0200

    ajout out.js

--- out.js
+++ out.js
@@ -1,37 +1,110 @@
 "use strict";
 (() => {
-  // js/Commands.js
-  var Command = class {
-    constructor(string) {
-      this.executed = false;
-      this.command = string;
-    }
-    execute(string) {
-      if (string == this.command) {
-        console.log("CA MARCHE");
+  // js/Quest.js
+  var Quest = class {
+    constructor(commandsParam = []) {
+      this.finished = false;
+      this.executed = [];
+      for (let i = 0; i < commandsParam.length; i++) {
+        this.executed[i] = false;
       }
+      this.commands = commandsParam;
+    }
+    /*addCommand(command){
+        this.commands.push(command);
+    }*/
+    /*
+     executeCmd(string){
+         if(!this.finished){
+             let i = this.getCurrent();
+             this.commands[i].execute(string);
+         }
+     }*/
+    getFinished() {
+      return this.finished;
+    }
+    getCurrent() {
+      let i;
+      for (i = 0; i < this.commands.length; i++) {
+        if (this.executed[i] == false) {
+          break;
+        }
+      }
+      return i;
+    }
+    updateStatus() {
+      for (let i = 0; i < this.commands.length; i++) {
+        if (this.executed[i] == false) {
+          this.finished = false;
+          return;
+        }
+      }
+      this.finished = true;
+    }
+    /*checkVar(string){
+        let string1 = new String(string).replace(/\s+/g, ' ').trim();
+        let cmdCur =  new String(this.commands[this.getCurrent()]);
+        let string2 =cmdCur.replace(/\s+/g, ' ').trim();
+        console.log("list commnades "+ this.commands);
+        console.log("cmdCur" + cmdCur);
+        console.log("listString" + string);
+        if(string1== string2){
+            return true;
+        }
+        return false;
+    }*/
+    checkVar(string) {
+      let string1 = string.replace(/\s+/g, " ").trim();
+      let cmdCur = this.commands[this.getCurrent()];
+      let string2 = cmdCur.replace(/\s+/g, " ").trim();
+      if (string1 === string2) {
+        this.executed[this.getCurrent()] = true;
+        this.updateStatus();
+      }
+      return string1 === string2;
     }
   };
 
-  // js/Quest.js
-  var Quest = class {
+  // js/Commands.js
+  var Command = class {
     constructor() {
-      this.finished = false;
-      this.commands = new Array();
     }
-    addCommand(command) {
-      this.commands.push(command);
+    execute() {
     }
-    executeCmd(string) {
-      if (!this.finished) {
-        for (i = 0; i < this.commands.length; i++) {
-          if (cmd.executed == false) {
-            i--;
-            break;
-          }
-        }
-        this.commands[i].execute(string);
+  };
+  var Script = class extends Command {
+    constructor() {
+      super();
+    }
+    execute(string) {
+      let res = "";
+      switch (string.split(" ")[1]) {
+        case "altf4.sh":
+          res = "Cr\xE9dits:\nAmaglio Matias: son +\nArcelin Nino: htlm/scss et devellopement backend\nPonroy C\xE9lie: devellopement backend--------------------------------------------------";
+          break;
+        case "help.sh":
+          res = "HAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+          break;
+        case "nino.sh":
+          res = "nino to the rescue";
+        case "matias.sh":
+          res = "you bought a cookie to matias he will help you";
+          break;
+        case "celie.sh":
+          res = "its all good celie helped you :)";
+          break;
       }
+    }
+  };
+  var Nano = class extends Command {
+    constructor() {
+      super();
+    }
+    execute(string) {
+      if (string) {
+        return true;
+      } else
+        return false;
     }
   };
 
@@ -39,17 +112,41 @@
   var TheLastResort = class {
     constructor() {
       this.current = 0;
-      this.quests = new Array();
+      this.quests = [];
     }
-    addQuests(quest) {
+    addQuest(quest) {
+      this.updateCurrent();
       this.quests.push(quest);
     }
     executeCmd(string = "") {
-      this.quests[this.current].executeCmd();
+      let command = string.split(" ");
+      switch (command[0]) {
+        case "nano":
+          new Nano().execute(string);
+          break;
+        case "modify":
+          this.quests[this.current].checkVar(string);
+          break;
+        case "run":
+          new Script().execute(string);
+          break;
+      }
+    }
+    updateCurrent() {
+      let i = 0;
+      for (i = 0; i < this.quests.length; i++) {
+        if (this.quests[i].getFinished() == false) {
+          break;
+        }
+      }
+      this.current = i;
     }
   };
 
   // js/main.js
-  theLastResort = new TheLastResort();
-  theLastResort.add(new Quest().addCommand(new Command("hello")));
+  var theLastResort = new TheLastResort();
+  var quest1 = new Quest(["modify hello = true"]);
+  console.log(quest1);
+  theLastResort.addQuest(quest1);
+  theLastResort.executeCmd("modify hello = true");
 })();
